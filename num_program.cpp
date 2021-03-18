f49ce22259f082dc9c0f21700fb9f6f61801e285
Scarred healer <devansh.shrivastava55@gmail.com>
2021-03-18 14:43:45 +0530

    Create num_program.cpp
    Program for adding two numbers from user input

---
+++ num_program.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+int num1,num2,sum;
+using namespace std;
+cout<<"\nEnter value for num1 : ";
+cin >> num1;
+cout << "\nEnter value for num 2 : ";
+cin >> num2;
+sum = num1+num2;
+cout<< "\nThe sum of the numbers is : " << sum;
+return 0;
+}
